71ac4a0d76a7f1179b07c8b896864db41a9dfb3f
Robin Guest <robin.verhagen.giuest@gmail.com>
2025-11-25 19:06:28 -0500

    Pattern is now generated programmatically in C#. Unshown bytes of layer 2 images are filled with random colours.

--- src/csharp/PatternGen/BankExtensions.cs
+++ src/csharp/PatternGen/BankExtensions.cs
@@ -1,36 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+
 namespace PatternGen
 {
     public static class BankExtensions
     {
         const int BANK_SIZE = 8 * 1024;
 
-        public static Dictionary<int, byte[]> Setup(this Dictionary<int, byte[]> dic, int bankNo, byte colour)
+        public static Dictionary<int, byte[]> Setup(this Dictionary<int, byte[]> banks, int bankNo, byte colour)
         {
             var bytes = new byte[BANK_SIZE];
             for (int i = 0; i < BANK_SIZE; i++)
                 bytes[i] = colour;
-            dic.Add(bankNo, bytes);
-            return dic;
+            banks.Add(bankNo, bytes);
+            return banks;
         }
 
-        public static Dictionary<int, byte[]> Fill(this Dictionary<int, byte[]> dic, int bankA, int bankB,
+        public static Dictionary<int, byte[]> FillRandom8KbBanks(this Dictionary<int, byte[]> banks, int startBank, int bankCount, 
+            int seed, params byte[] colours)
+        {
+            var rng = new Random(seed);
+            for (int i = 0; i < bankCount; i++)
+            {
+                var bank = new byte[BANK_SIZE];
+                for(int j = 0; j < bank.Length; j++)
+                    bank[j] = colours[rng.Next(colours.Length)];
+                banks.Add(startBank++, bank);
+            }
+            return banks;
+        }
+
+        public static Dictionary<int, byte[]> Fill(this Dictionary<int, byte[]> banks, int bankA, int bankB,
             int x, int y, byte colorA, byte colorB, int firstA, int firstB, int repeatA, int repeatB)
         {
             int addr = (x * 256) + y;
             int end = addr + 256;
             for (int i = 0; i < firstA; i++)
             {
-                dic[bankA][addr++] = colorA;
+                banks[bankA][addr++] = colorA;
             }
             for (int i = 0; i < firstB; i++)
             {
-                dic[bankB][addr++] = colorB;
+                banks[bankB][addr++] = colorB;
             }
             bool done = false;
             while (!done)
@@ -42,7 +56,7 @@ namespace PatternGen
                         done = true;
                         break;
                     }
-                    dic[bankA][addr++] = colorA;
+                    banks[bankA][addr++] = colorA;
                 }
                 for (int i = 0; i < repeatB; i++)
                 {
@@ -51,13 +65,63 @@ namespace PatternGen
                         done = true;
                         break;
                     }
-                    dic[bankB][addr++] = colorB;
+                    banks[bankB][addr++] = colorB;
                 }
             }
-            return dic;
+            return banks;
         }
 
-        public static Dictionary<int, byte[]> SaveAsPng8_320x256(this Dictionary<int, byte[]> dic, int startBank, string fileName)
+        public static Dictionary<int, byte[]> CreatePattern(this Dictionary<int, byte[]> banks, int startBankA, int startBankB,
+            byte colorA, byte colorB, byte colorC)
+        {
+            var patterns = Pattern.Create();
+            for (int x = 0; x < 320; x++)
+            {
+                int y = 0;
+                int bankA = startBankA + (x / 32);
+                int bankB = startBankB + (x / 32);
+                int addr = ((x % 32) * 256) + y;
+                int end = addr + 256;
+                var pattern = patterns[(x / 2) % patterns.Length];
+                int bankSeq = x / 32;
+                byte colorEven = ((bankSeq / 2) * 2) == bankSeq ? colorA : colorB;
+                byte color = ((x / 2) * 2) == x ? colorEven : colorC;
+
+                for (int i = 0; i < pattern.FirstA; i++)
+                {
+                    banks[bankA][addr++] = color;
+                }
+                for (int i = 0; i < pattern.FirstB; i++)
+                {
+                    banks[bankB][addr++] = color;
+                }
+                bool done = false;
+                while (!done)
+                {
+                    for (int i = 0; i < pattern.RepeatA; i++)
+                    {
+                        if (addr >= end)
+                        {
+                            done = true;
+                            break;
+                        }
+                        banks[bankA][addr++] = color;
+                    }
+                    for (int i = 0; i < pattern.RepeatB; i++)
+                    {
+                        if (addr >= end)
+                        {
+                            done = true;
+                            break;
+                        }
+                        banks[bankB][addr++] = color;
+                    }
+                }
+            }
+            return banks;
+        }
+
+        public static Dictionary<int, byte[]> SaveAsPng24_320x256(this Dictionary<int, byte[]> banks, int startBank, string fileName)
         {
             var pal = new Dictionary<byte, Color>();
             using (var img = new Bitmap(320, 256))
@@ -80,7 +144,7 @@ namespace PatternGen
                             // Get the bank and byte corresponding to the pixel
                             int bank = startBank + (x / 32);
                             int byteIndex = ((x % 32) * 256) + y;
-                            byte pixel = dic[bank][byteIndex];
+                            byte pixel = banks[bank][byteIndex];
                             if (!pal.ContainsKey(pixel))
                             {
                                 // Inside these two loops, we can set each pixel in turn
@@ -88,7 +152,7 @@ namespace PatternGen
                                 // Assume pixel in bank is %RRRGGGBB if we don't have an explicit palette
                                 int r3 = (pixel >> 5) & 0b111;
                                 int g3 = (pixel >> 2) & 0b111;
-                                int b3 = (pixel << 1) & 0b111;
+                                int b3 = (pixel << 1) & 0b110;
                                 b3 = b3 == 0 ? b3 : (b3 | 0b1);
                                 int r8 = (r3 << 5) | (r3 << 2) | (r3 >> 1);
                                 int g8 = (g3 << 5) | (g3 << 2) | (g3 >> 1);
@@ -100,9 +164,21 @@ namespace PatternGen
                     }
                     break;
                 }
+                fileName = fileName.Replace("{startBank}", startBank.ToString())
+                    .Replace("{endBank}", (startBank + 9).ToString());
                 img.Save(fileName, ImageFormat.Png);
             }
-            return dic;
+            return banks;
+        }
+
+        public static Dictionary<int, byte[]> SaveAs8KbBanks(this Dictionary<int, byte[]> banks, string fileName)
+        {
+            foreach (int bankNo in banks.Keys)
+            {
+                string fn = fileName.Replace("{bankNo}", bankNo.ToString());
+                File.WriteAllBytes(fn, banks[bankNo]);
+            }
+            return banks;
         }
     }
 }
--- src/csharp/PatternGen/Program.cs
+++ src/csharp/PatternGen/Program.cs
@@ -1,134 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PatternGen
 {
     internal class Program
     {
-        const string OUTPUT_PATH = @"..\..\..\..\..\data\";
+        const string IMAGE_PATH = @"..\..\..\..\..\images\resized\";
+        const string DATA_PATH = @"..\..\..\..\..\data\";
+        const int SEED = 545345;
+
         const byte BLACK   = 0b00000000; // 0
         const byte BLUE    = 0b00000011; // 1
         const byte RED     = 0b11100000; // 2
         const byte MAGENTA = 0b11100011; // 3
         const byte GREEN   = 0b00011100; // 4
         const byte CYAN    = 0b00011111; // 5
-        const byte YELLOW  = 0b11111100; // 6
+        const byte YELLOW_ = 0b11111100; // 6
         const byte WHITE   = 0b11111111; // 7
 
         static void Main()
         {
-            var banks = new Dictionary<int, byte[]>();
-            int bank = 18;
-            banks.Setup(bank++, BLUE)  // Fill 10x 8K layer 2 banks
-                .Setup(bank++, GREEN)  // with alternate blue / green stripes
-                .Setup(bank++, BLUE)
-                .Setup(bank++, GREEN)
-                .Setup(bank++, BLUE)
-                .Setup(bank++, GREEN)
-                .Setup(bank++, BLUE)
-                .Setup(bank++, GREEN)
-                .Setup(bank++, BLUE)
-                .Setup(bank++, GREEN)
-
-                .Setup(bank++, YELLOW) // Fill 10x 8K layer 2 banks
-                .Setup(bank++, WHITE)  // with alternate yellow/white stripes
-                .Setup(bank++, YELLOW)
-                .Setup(bank++, WHITE)
-                .Setup(bank++, YELLOW)
-                .Setup(bank++, WHITE)
-                .Setup(bank++, YELLOW)
-                .Setup(bank++, WHITE)
-                .Setup(bank++, YELLOW)
-                .Setup(bank++, WHITE);
-
-            // 18, 28
-            
-            banks.Fill(18, 28, 00, 0, RED,   RED,   1, 3, 2, 3);
-            banks.Fill(18, 28, 01, 0, BLACK, BLACK, 1, 3, 2, 3);
-            banks.Fill(18, 28, 02, 0, RED,   RED,   2, 2, 3, 2);
-            banks.Fill(18, 28, 03, 0, BLACK, BLACK, 2, 2, 3, 2);
-            banks.Fill(18, 28, 04, 0, RED,   RED,   0, 0, 2, 3);
-            banks.Fill(18, 28, 05, 0, BLACK, BLACK, 0, 0, 2, 3);
-            banks.Fill(18, 28, 06, 0, RED,   RED,   0, 0, 3, 2);
-            banks.Fill(18, 28, 07, 0, BLACK, BLACK, 0, 0, 3, 2);
-            
-            banks.Fill(18, 28, 08, 0, RED,   RED,   0, 1, 2, 3);
-            banks.Fill(18, 28, 09, 0, BLACK, BLACK, 0, 1, 2, 3);
-            banks.Fill(18, 28, 10, 0, RED,   RED,   0, 1, 3, 2);
-            banks.Fill(18, 28, 11, 0, BLACK, BLACK, 0, 1, 3, 2);
-            banks.Fill(18, 28, 12, 0, RED,   RED,   0, 2, 2, 3);
-            banks.Fill(18, 28, 13, 0, BLACK, BLACK, 0, 2, 2, 3);
-            banks.Fill(18, 28, 14, 0, RED,   RED,   0, 2, 3, 2);
-            banks.Fill(18, 28, 15, 0, BLACK, BLACK, 0, 2, 3, 2);
-            
-            banks.Fill(18, 28, 16, 0, RED,   RED,   0, 3, 2, 3);
-            banks.Fill(18, 28, 17, 0, BLACK, BLACK, 0, 3, 2, 3);
-            banks.Fill(18, 28, 18, 0, RED,   RED,   1, 2, 3, 2);
-            banks.Fill(18, 28, 19, 0, BLACK, BLACK, 1, 2, 3, 2);
-            banks.Fill(18, 28, 20, 0, RED,   RED,   1, 3, 2, 3);
-            banks.Fill(18, 28, 21, 0, BLACK, BLACK, 1, 3, 2, 3);
-            banks.Fill(18, 28, 22, 0, RED,   RED,   2, 2, 3, 2);
-            banks.Fill(18, 28, 23, 0, BLACK, BLACK, 2, 2, 3, 2);
-
-            banks.Fill(18, 28, 24, 0, RED,   RED,   0, 0, 2, 3);
-            banks.Fill(18, 28, 25, 0, BLACK, BLACK, 0, 0, 2, 3);
-            banks.Fill(18, 28, 26, 0, RED,   RED,   0, 0, 3, 2);
-            banks.Fill(18, 28, 27, 0, BLACK, BLACK, 0, 0, 3, 2);
-            banks.Fill(18, 28, 28, 0, RED,   RED,   0, 1, 2, 3);
-            banks.Fill(18, 28, 29, 0, BLACK, BLACK, 0, 1, 2, 3);
-            banks.Fill(18, 28, 30, 0, RED,   RED,   0, 1, 3, 2);
-            banks.Fill(18, 28, 31, 0, BLACK, BLACK, 0, 1, 3, 2);
-
-            // 19, 29
-
-            banks.Fill(19, 29, 00, 0, MAGENTA, MAGENTA, 0, 2, 2, 3);
-            banks.Fill(19, 29, 01, 0, BLACK,   BLACK,   0, 2, 2, 3);
-            banks.Fill(19, 29, 02, 0, MAGENTA, MAGENTA, 0, 2, 3, 2);
-            banks.Fill(19, 29, 03, 0, BLACK,   BLACK,   0, 2, 3, 2);
-            banks.Fill(19, 29, 04, 0, MAGENTA, MAGENTA, 0, 3, 2, 3);
-            banks.Fill(19, 29, 05, 0, BLACK,   BLACK,   0, 3, 2, 3);
-            banks.Fill(19, 29, 06, 0, MAGENTA, MAGENTA, 1, 2, 3, 2);
-            banks.Fill(19, 29, 07, 0, BLACK,   BLACK,   1, 2, 3, 2);
-
-            banks.Fill(19, 29, 08, 0, MAGENTA, MAGENTA, 1, 3, 2, 3);
-            banks.Fill(19, 29, 09, 0, BLACK,   BLACK,   1, 3, 2, 3);
-            banks.Fill(19, 29, 10, 0, MAGENTA, MAGENTA, 2, 2, 3, 2);
-            banks.Fill(19, 29, 11, 0, BLACK,   BLACK,   2, 2, 3, 2);
-            banks.Fill(19, 29, 12, 0, MAGENTA, MAGENTA, 0, 0, 2, 3);
-            banks.Fill(19, 29, 13, 0, BLACK,   BLACK,   0, 0, 2, 3);
-            banks.Fill(19, 29, 14, 0, MAGENTA, MAGENTA, 0, 0, 3, 2);
-            banks.Fill(19, 29, 15, 0, BLACK,   BLACK,   0, 0, 3, 2);
-
-            banks.Fill(19, 29, 16, 0, MAGENTA, MAGENTA, 0, 1, 2, 3);
-            banks.Fill(19, 29, 17, 0, BLACK,   BLACK,   0, 1, 2, 3);
-            banks.Fill(19, 29, 18, 0, MAGENTA, MAGENTA, 0, 1, 3, 2);
-            banks.Fill(19, 29, 19, 0, BLACK,   BLACK,   0, 1, 3, 2);
-            banks.Fill(19, 29, 20, 0, MAGENTA, MAGENTA, 0, 2, 2, 3);
-            banks.Fill(19, 29, 21, 0, BLACK,   BLACK,   0, 2, 2, 3);
-            banks.Fill(19, 29, 22, 0, MAGENTA, MAGENTA, 0, 2, 3, 2);
-            banks.Fill(19, 29, 23, 0, BLACK,   BLACK,   0, 2, 3, 2);
-
-            banks.Fill(19, 29, 24, 0, MAGENTA, MAGENTA, 0, 3, 2, 3);
-            banks.Fill(19, 29, 25, 0, BLACK,   BLACK,   0, 3, 2, 3);
-            banks.Fill(19, 29, 26, 0, MAGENTA, MAGENTA, 1, 2, 3, 2);
-            banks.Fill(19, 29, 27, 0, BLACK,   BLACK,   1, 2, 3, 2);
-            banks.Fill(19, 29, 28, 0, MAGENTA, MAGENTA, 1, 3, 2, 3);
-            banks.Fill(19, 29, 29, 0, BLACK,   BLACK,   1, 3, 2, 3);
-            banks.Fill(19, 29, 30, 0, MAGENTA, MAGENTA, 2, 2, 3, 2);
-            banks.Fill(19, 29, 31, 0, BLACK,   BLACK,   2, 2, 3, 2);
-
-            banks.SaveAsPng8_320x256(18, Path.Combine(OUTPUT_PATH, $"pattern_18-27.png"));
-            banks.SaveAsPng8_320x256(28, Path.Combine(OUTPUT_PATH, $"pattern_28-37.png"));
-
-            foreach (var key in banks.Keys)
-            {
-                string fn = Path.Combine(OUTPUT_PATH, $"pattern_{key}.bin");
-                File.WriteAllBytes(fn, banks[key]);
-            }
+            new Dictionary<int, byte[]>()
+                .FillRandom8KbBanks(18, 20, SEED, BLACK, WHITE)
+                .CreatePattern(18, 28, RED, MAGENTA, BLACK)
+                .SaveAsPng24_320x256(18, Path.Combine(IMAGE_PATH, "pattern_{startBank}-{endBank}.png"))
+                .SaveAsPng24_320x256(28, Path.Combine(IMAGE_PATH, "pattern_{startBank}-{endBank}.png"))
+                .SaveAs8KbBanks(Path.Combine(DATA_PATH, "pattern_{bankNo}.bin"));
         }
     }
 }
